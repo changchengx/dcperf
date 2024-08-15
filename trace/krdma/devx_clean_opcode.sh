17930bb0da4b5d243fdf973ca78c5826f0d65676
Liu, Changcheng <changcheng.liu@aliyun.com>
2024-08-15 11:07:18 -0500

    trace: trace devx_obj_cleanup
    Signed-off-by: Liu, Changcheng <changcheng.liu@aliyun.com>

--- trace/krdma/devx_clean_opcode.sh
+++ trace/krdma/devx_clean_opcode.sh
@@ -7,6 +7,11 @@ cd /sys/kernel/debug/tracing
 #uobject: arg1
 #obj: +24($arg1)
 #opcode: obj->inbox, +20(obj)
+#obj_type: obj->inbox[6], +6(+20(obj))( not work, need to use +26(obj) )
 #flags: obj->flags, +84(obj)
-echo 'p:mlx5_ib/clean mlx5_ib:devx_obj_cleanup opcode=+20(+24($arg1)):x16 obj_type=+6(+20(+24($arg1))):x16 flags=+84(+24($arg1)):x32' > kprobe_events
+echo 'p:mlx5_ib/clean mlx5_ib:devx_obj_cleanup opcode=+20(+24($arg1)):x16 obj_type=+26(+24($arg1)):x16 flags=+84(+24($arg1)):x32' > kprobe_events
 echo 1 > events/mlx5_ib/clean/enable
+
+#How to disable:
+# # echo 0 > /sys/kernel/debug/tracing/events/mlx5_ib/clean/enable
+# # echo '-:mlx5_ib/clean' > /sys/kernel/debug/tracing/kprobe_events
