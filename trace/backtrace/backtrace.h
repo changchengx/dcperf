523f391262364831b5696e3ff8c2feec75e7a066
Liu, Changcheng <changcheng.liu@aliyun.com>
2026-05-10 17:52:19 +0800

    function to support stack backtrace
    Signed-off-by: Liu, Changcheng <changcheng.liu@aliyun.com>

---
+++ trace/backtrace/backtrace.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2026 Liu, Changcheng <changcheng.liu@aliyun.com>
+ */
+
+#include <stdio.h>
+extern void debug_print_backtrace(FILE *stream, int strip);
+
+// vim: ts=4 sw=4 expandtab fileencoding=utf-8 ft=c
